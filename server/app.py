ba4c16d28f9e7aabaa86be613bac9d156bfa2a68
Joy Njoroge <njorogejoy78@gmail.com>
2025-07-03 13:51:52 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -14,52 +14,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages', methods = ['GET', 'POST'])
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    if request.method == "GET":
-        msgs = Message.query.order_by(Message.created_at.asc()).all()
-        return make_response([m.to_dict() for m in msgs], 200)
-
-
-    data = request.get_json(silent=True) or request.form
-
-    body     = data.get("body")
-    username = data.get("username")
-
-    if not body or not username:
-        return make_response(
-            {"error": "body and username required"}, 400
+    if request.method == 'GET':
+        messages = Message.query.order_by('created_at').all()
+        
+        return make_response([message.to_dict() for message in messages],200 )
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username=data['username']
         )
 
-    new_msg = Message(body=body, username=username)
-    db.session.add(new_msg)
-    db.session.commit()
-
-    return make_response(new_msg.to_dict(), 201)
-
-@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
-def messages_by_id(id):
-    msg = Message.query.get(id)
-    if not msg:
-        return make_response({"error": "Message not found"}, 404)
-
-
-    if request.method == "GET":
-        return make_response(msg.to_dict(), 200)
-
-    if request.method == "PATCH":
-        data = request.get_json(silent=True) or request.form
-        if "body" in data:
-            msg.body = data["body"]
-            db.session.add(msg)
-            db.session.commit()
-        return make_response(msg.to_dict(), 200)
-
-
-    if request.method == "DELETE":
-        db.session.delete(msg)
+        db.session.add(message)
         db.session.commit()
-        return make_response({}, 204)
 
-if __name__ == '__main__':
-    app.run(port=5555)
+        return  make_response(message.to_dict(),  201,)
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
+def messages_by_id(id):
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+            
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(message.to_dict(),200 )
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        return make_response( {'deleted': True} , 200)
